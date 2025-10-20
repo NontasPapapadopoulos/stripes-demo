91343bff8577c883e5ec5e77f90cf66168d76970
n.papadopoulos <n.papadopoulos@Hyperiosoftware.com>
2025-10-20 09:54:11 +0300

    configure scan screen.

--- app/src/main/java/com/example/stripesdemo/presentation/ui/screen/scan/ScanScreen.kt
+++ app/src/main/java/com/example/stripesdemo/presentation/ui/screen/scan/ScanScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Check
@@ -31,8 +33,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.stripesdemo.presentation.ui.icons.Barcode
@@ -128,7 +132,7 @@ private fun ScanContent(
         Column(
             modifier = Modifier.fillMaxSize()
                 .padding(it)
-                .padding(24.dp),
+                .padding(horizontal = 24.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -143,27 +147,42 @@ private fun ScanContent(
                 modifier = Modifier.fillMaxWidth()
             )
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
             OutlinedTextField(
                 value = state.count,
-                onValueChange = { onCountChanged(it) },
+                onValueChange = { count ->
+                    if (count.isDigitsOnly())
+                        onCountChanged(count)
+                                },
                 label = {
                     Text(text = "Count")
                 },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        if (state.count.isNotEmpty() && state.barcode.isNotEmpty()) {
+                            submitScan()
+                        }
+                    }
+                ),
+                singleLine = true,
                 modifier = Modifier.fillMaxWidth()
             )
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
             Button(
                 onClick = submitScan,
+                enabled = state.barcode.isNotEmpty() && state.count.isNotEmpty() && state.count.isDigitsOnly(),
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text(text = "Submit Scan")
             }
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
             Text(
                 text = "Total Scans: ${state.numberOfScans}",
